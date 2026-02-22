e0b0fea1254d312629261fd57b0eefc1e8f17b58
Niladri-Gudu <niladrigudu@gmail.com>
2026-02-22 11:53:39 +0530

    updated db schema

---
+++ packages/database/prisma/migrations/20260222062304_dev/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "IndexerState" (
+    "id" TEXT NOT NULL DEFAULT 'main',
+    "lastIndexedBlock" INTEGER NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "IndexerState_pkey" PRIMARY KEY ("id")
+);
